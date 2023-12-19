b8b6f7c2c480751deaf8f063ff9db3b1830aeab5
gharby.moez <gharby.moez@outlook.fr>
2023-12-19 02:06:13 +0100

    test deleted

--- AnimalCountingDatabase.Tests/UnitTest1.cs
+++ AnimalCountingDatabase.Tests/UnitTest1.cs
@@ -1,8 +1,3 @@
-using LiveCodeCICD;
-using LiveCodeCICD.Controllers;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-
 namespace AnimalCountingDatabase.Tests
 {
     public class UnitTest1
@@ -12,35 +7,35 @@ namespace AnimalCountingDatabase.Tests
         {
             Assert.True(1 == 1);
         }
-        [Fact]
-        public async Task IntegrationTest()
-        {
-            //Create db 
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables()
-                .Build();
+        /* [Fact]
+         public async Task IntegrationTest()
+         {
+             //Create db 
+             var configuration = new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json")
+                 .AddEnvironmentVariables()
+                 .Build();
 
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            optionsBuilder
-                // Uncomment the following line if you want to print generated
-                // SQL statements on the console.
-                // .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
-                .UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+             optionsBuilder
+                 // Uncomment the following line if you want to print generated
+                 // SQL statements on the console.
+                 // .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+                 .UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
 
-            var context = new ApplicationDbContext(optionsBuilder.Options);
+             var context = new ApplicationDbContext(optionsBuilder.Options);
 
-            //DElete database
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
-            //create controller
-            var controller = new CustomersController(context);
-            //Add Customer
-            await controller.Add(new Customer { Name = "Alex" });
-            //Assert result
+             //DElete database
+             await context.Database.EnsureDeletedAsync();
+             await context.Database.EnsureCreatedAsync();
+             //create controller
+             var controller = new CustomersController(context);
+             //Add Customer
+             await controller.Add(new Customer { Name = "Alex" });
+             //Assert result
 
-            var result = await controller.GetAll();
-            Assert.Equal(1, result.Count());
-        }
+             var result = await controller.GetAll();
+             Assert.Equal(1, result.Count());
+         }*/
     }
 }
